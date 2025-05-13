88d4797b4abe30375077d074daf0f89fa7f63ff0
thouderth <toudherth.mekhnache@gmail.com>
2025-05-13 15:26:19 +0200

    modif model

---
+++ target/compiled/oms_dbt_proj/models/example/schema.yml/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select id
+from SLEEKMART_OMS.L3_CONSUMPTION.my_second_dbt_model
+where id is null
+
+
---
+++ target/run/oms_dbt_proj/models/generate_schema_name.sql
@@ -0,0 +1,7 @@
+
+  create or replace   view SLEEKMART_OMS.L3_CONSUMPTION.generate_schema_name
+  
+   as (
+    
+  );
+
